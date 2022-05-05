e0d86be3eca76e69c2b90f205ba60f6e384ce31a
Ivan <zhdankin.ivan@gmail.com>
2022-05-05 18:13:41 +0100

    test commit

--- MonteCarloSimulationVaREngine/MonteCarloSimulationVaREngine/MonteCarloSimulationVaREngine.cpp
+++ MonteCarloSimulationVaREngine/MonteCarloSimulationVaREngine/MonteCarloSimulationVaREngine.cpp
@@ -35,7 +35,7 @@
 
 int main()
 {
-    //Tutorial 5
+    //Tutorial 5 dfdf
     unsigned long riskFactorDaysUsed = 250;
     //sdfsdf
     //Read file to get the values of risk factors, make sure the file is located in same folder as the project
